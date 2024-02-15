3f149e7f012bd9e4403f635de09287aace8656f6
Annie Booth <annie_booth@ncsu.edu>
2024-02-15 11:50:01 -0500

    simplify matern kernel optimization

--- dgp.hm/R/misc.R
+++ dgp.hm/R/misc.R
@@ -10,41 +10,35 @@
 # Optimize Matern -------------------------------------------------------------
 #' @export
 
-opt_matern <- function(dx, y, sdd, init = c(0.1, -5, 0.1, 0.1), 
-                       lower = c(-5, -35, -5, 0.2),
-                       upper = c(4, 3, 4, 100)) { 
+opt_matern <- function(dx, y, sdd, init = c(0.1, 0.1), lower = c(-5, -5),
+                       upper = c(4, 4)) { 
   out <- optim(init, nl_matern, method = "L-BFGS-B", lower = lower,
                upper = upper, dx = dx, y = y, sdd = sdd)
-  return(list(kappa_hat = out$par[4], 
-              phi_hat = exp(out$par[1]),
-              theta_hat = exp(out$par[1]) * out$par[4], 
-              g_hat = exp(out$par[2]),
-              tau2_hat = exp(out$par[3])))
+  return(list(theta_hat = exp(out$par[1]), 
+              tau2_hat = exp(out$par[2])))
 }
 
 # Negative log likelihood Matern ----------------------------------------------
 
 nl_matern <- function(par, dx, y, sdd) {
-  # par: c(theta, g, tau2, kappa)
+  # par: c(theta, tau2)
   # dx: squared distances of inputs
   # y: vector of response
   # sdd: scaled precisions
+  # UPDATED: fixed kappa = 2.5 and g = 1e-8, used deepgp instead of geo
   
   theta <- exp(par[1])
-  g <- exp(par[2])
-  tau2 <- exp(par[3])
-  kappa <- par[4]
+  tau2 <- exp(par[2])
   n <- nrow(y)
   d <- ncol(y)
   
-  K <- tau2 * (geoR::matern(sqrt(dx), phi = theta, kappa = kappa) + diag(g, n))
-  Ki <- solve(K)
-  ldetK <- determinant(K, logarithm = TRUE)$modulus
-  
+  K <- deepgp:::Matern(dx, tau2, theta, 1e-8, 2.5)
+  id <- deepgp::invdet(K)
+
   ll <- 0
   for (i in 1:d) {
     yit <- y[, i] / sdd
-    ll <- ll - (1/2)*(t(yit) %*% Ki %*% yit) - (1/2)*ldetK 
+    ll <- ll - (1/2) * (t(yit) %*% id$Mi %*% yit) - (1/2) * id$ldet
   }
   return(-ll)
 }
