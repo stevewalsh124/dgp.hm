cb5e3bda421be1551bf33f339170caf572ce6482
Steve Walsh <walsh124@vt.edu>
2024-03-01 14:38:58 -0500

    collect all training and test post means; init commit designs for training and test

--- fitting/collect_means.R
+++ fitting/collect_means.R
@@ -8,8 +8,8 @@
 ###############################################################################
 
 # Training models
-post_means <- data.frame(matrix(NA, nrow = 351, ncol = 10)) # CHANGE TO 111 ONCE THEY ARE RUN
-for (i in 1:10) {
+post_means <- data.frame(matrix(NA, nrow = 351, ncol = 111))
+for (i in 1:111) {
   model_name <- paste0("M", if (i < 100) {"0"}, if (i < 10) {"0"}, i) 
   results <- read.csv(paste0("results/dgp_", model_name, ".csv"))
   post_means[, i] <- results$m
@@ -17,13 +17,13 @@ for (i in 1:10) {
 }
 write.csv(post_means, "results/post_means_train.csv", row.names = FALSE)
 
-# Testing models (STILL NEED TO BE RUN FIRST)
-#post_means <- data.frame(matrix(NA, nrow = 351, ncol = 7))
-#test_names <- c("M000", "E001", "E002", "E003", "E009", "E010")
-#for (i in 1:7) {
-#  model_name <- test_names[i]
-#  results <- read.csv(paste0("results/dgp_", model_name, ".csv"))
-#  post_means[, i] <- results$m
-#  colnames(post_means)[i] <- model_name
-#}
-#write.csv(post_means, "post_means_test.csv", row.names = FALSE)
+# Testing models
+post_means <- data.frame(matrix(NA, nrow = 351, ncol = 6))
+test_names <- c("M000", "E001", "E002", "E003", "E009", "E010")
+for (i in 1:6) {
+ model_name <- test_names[i]
+ results <- read.csv(paste0("results/dgp_", model_name, ".csv"))
+ post_means[, i] <- results$m
+ colnames(post_means)[i] <- model_name
+}
+write.csv(post_means, "results/post_means_test.csv", row.names = FALSE)
