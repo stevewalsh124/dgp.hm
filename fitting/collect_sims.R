2e8ec6c067e39523f810a15c14f4b442a7e8823f
Stephen Walsh <stevewalsh124@yahoo.com>
2024-06-04 01:54:52 -0400

    update results for fixed f2; make boxplots to match draft

--- fitting/collect_sims.R
+++ fitting/collect_sims.R
@@ -20,3 +20,17 @@ for(model in 1:2){
 
 boxplot(results, names = c("1,A","1,B","1,C","2,A","2,B","2,C"), 
         ylab="Average MSE", main = "DGP-HM MSE across models/settings")
+
+# Boxplot for f1
+boxplot(results[1:3], ylim=c(0,.018), main="mses: function 1",
+        names = c("1,A","1,B","1,C"), ylab="MSE")
+grid(nx = NULL, ny = NULL,
+     col = "#ebebeb", lwd = 2, lty=1)
+boxplot(results[1:3], add = TRUE, names=F)
+
+# Boxplot for f2
+boxplot(results[4:6], ylim=c(0,.005), main="mses: function 2",
+        names = c("2,A","2,B","2,C"), ylab="MSE")
+grid(nx = NULL, ny = NULL,
+     col = "#ebebeb", lwd = 2, lty=1)
+boxplot(results[4:6], add = TRUE, names = F)
