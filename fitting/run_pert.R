daabbc6ed95298efe15d655058eef79531e83679
Stephen Walsh <stevewalsh124@yahoo.com>
2024-06-06 10:22:12 -0400

    load library

--- fitting/run_pert.R
+++ fitting/run_pert.R
@@ -1,5 +1,7 @@
 # Power Spectra Simulations
 
+library(dgp.hm)
+
 # From the "linear" folder
 sim <- read.table("../CosmicEmu_etc/32 Models Mira Titan/linear/RUN1/oneh_matterpower.dat")
 plot(log10(sim$V1), scrP(sim$V2,sim$V1), type="l",
@@ -21,7 +23,6 @@ plot(log10(sim2$V1)[sim2$V1<=1], scrP(sim2$V2, sim2$V1)[sim2$V1<=1],
      main = "emu, cutoff at k=1", type="l", xlim=c(-2.25,0),ylim=c(-4.3,-2.6))
 for (run in 12:42) {
   if(run == 27 | run == 28) next
-  print(run)
   if(run < 20)  folder <- ""
   if(run >= 20) folder <- "_2"
   if(run >= 30) folder <- "_3"
