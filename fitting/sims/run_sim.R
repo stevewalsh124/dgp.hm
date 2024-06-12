e03b5408e1fc092591205d63945edec3ca234e44
Annie Booth <annie_booth@ncsu.edu>
2024-06-12 10:19:16 -0400

    sim code ready to run

--- fitting/sims/run_sim.R
+++ fitting/sims/run_sim.R
@@ -1,13 +1,12 @@
 ###############################################################################
 # This script conducts a "bake-off" of various surrogate models under 2
 # different test functions (f1 and f2) and 4 different noise settings for 
-# a specified random seed.
+# a specified random seed.  All models are tasked with estimating the noise.
 #
 # Important Arguments:
 #   seed: random seed (must be set before sourcing functions.R)
 #   func: 1 or 2
 #   setting: 1, 2, 3, or 4 (indicates noise settings A-D from DPC paper)
-#   r: number of function realizations
 #
 # Output:
 #   data frame of MSE values for each model is either written to ".csv" in the
@@ -27,9 +26,8 @@ library(hetGP)
 library(mvtnorm)
 
 seed <- 1
-func <- 1
-setting <- 3
-r <- 5
+func <- 2
+setting <- 2
 args <- commandArgs(TRUE)
 if(length(args) > 0)
   for(i in 1:length(args))
@@ -40,11 +38,12 @@ if(!(setting %in% 1:4)) stop("setting should be 1, 2, 3, or 4")
 set.seed(seed)
 source("functions.R") # generates random model parameters upon sourcing
 
-vis <- FALSE # should plots be generated
+r <- 5 # number of replicates observed
+vis <- TRUE # should plots be generated
 
 # Generate data ---------------------------------------------------------------
 
-x <-seq(0, 4, by=0.1)
+x <- seq(0, 4, by=0.1)
 n <- length(x)
 Sigma_true <- get_Sigma_true(x, n, func, setting)
 if(vis) image(Sigma_true) # make sure sd plot looks right
@@ -61,26 +60,26 @@ y_avg <- colMeans(Y)
 if(func == 1) y_true <- f1(x, m1=m1, u1=u1)
 if(func == 2) y_true <- f2(x, m2=m2, u2=u2)
 
-# x_all <- NULL
-# y_all <- NULL
-# for(i in 1:r) {
-#   x_all <- c(x_all, x)
-#  y_all <- c(y_all, Y[i, ])
-# }
+x_all <- NULL
+y_all <- NULL
+for(i in 1:r) {
+  x_all <- c(x_all, x)
+  y_all <- c(y_all, Y[i, ])
+}
+if (vis) plot(x_all, y_all)
 
 # dgp.hm model ----------------------------------------------------------------
 
-# Previous option - estimate Sigma_hat (not used currently)
-# if(setting %in% 1:3) {
-#   var_y <- mean(apply(Y, 2, var))
-#   Sigma_hat = diag(var_y, n)
-# } else {
-#   params_hat <- opt_matern(D, t(Y), sdd =rep(1,n))
-#   Sigma_hat <- deepgp:::Matern(D, params_hat$tau2_hat, params_hat$theta_hat,
-#                                g=1e-8, v=2.5)
-# }
+if(setting %in% 1:3) {
+  var_y <- mean(apply(Y, 2, var))
+  Sigma_hat = diag(var_y, n)
+} else {
+  params_hat <- opt_matern(D, t(Y), sdd = rep(1,n))
+  Sigma_hat <- deepgp:::Matern(D, params_hat$tau2_hat, params_hat$theta_hat,
+                               g=1e-8, v=2.5)
+}
 
-fit <- dgp.hm::fit_two_layer_hm(x, y_avg, Sigma_hat = Sigma_true, nmcmc = 20000)
+fit <- dgp.hm::fit_two_layer_hm(x, y_avg, Sigma_hat = Sigma_hat, nmcmc = 20000)
 if(vis) plot(fit)
 fit <- dgp.hm::trim(fit, 15000, 5)
 fit <- dgp.hm::est_true(fit)
@@ -98,10 +97,9 @@ if (vis) {
 dgp_mse <- mean((fit$m - y_true)^2)
 
 # deepgp model ----------------------------------------------------------------
-# Not quite sure what to do here - use the average?  Or all the data?
 
-fit2 <- deepgp::fit_two_layer(x, y_avg, nmcmc = 10000, 
-                              true_g = mean(diag(Sigma_true)))
+
+fit2 <- deepgp::fit_two_layer(x_all, y_all, nmcmc = 10000)
 if(vis) plot(fit2)
 fit2 <- deepgp::trim(fit2, 5000, 5)
 fit2 <- predict(fit2, x)
@@ -109,7 +107,6 @@ if(vis) plot(fit2)
 deepgp_mse <- mean((fit2$mean - y_true)^2)
 
 # hetGP model -----------------------------------------------------------------
-# Not sure how to pass the true variance to this model....
 
 fit3 <- mleHetGP(matrix(x_all, ncol = 1), y_all, covtype = "Matern5_2")
 pred <- predict(matrix(x, ncol = 1), object = fit3)
