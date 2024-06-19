366c94de552f69d23f137a44ab402cfc55d85c2f
Annie Booth <annie_booth@ncsu.edu>
2024-06-19 18:38:05 -0400

    plot code

--- fitting/sims/collect_sims.R
+++ fitting/sims/collect_sims.R
@@ -1,11 +1,12 @@
 
-func <- 1
-setting <- 1
 r <- 5 
 
-par(mfrow = c(1, 2))
-for (setting in 1:2) {
-  filename <- paste0("results/sims_", func, "_", setting, "_", r, ".csv")
-  results <- read.csv(filename)[, -1]
-  boxplot(results)
+par(mfrow = c(2, 4))
+for (func in 1:2) {
+  for (setting in 1:4) {
+    filename <- paste0("results/sims_", func, "_", setting, "_", r, ".csv")
+    results <- read.csv(filename)[, -1]
+    boxplot(results, ylab = "MSE", 
+            main = paste0("Function ", func, " Setting ", setting))
+  }
 }
