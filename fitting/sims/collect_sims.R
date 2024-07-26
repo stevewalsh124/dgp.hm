40162b93b69d462163d09c9b1f0aa3c0a4882a75
Annie Booth <annie_booth@ncsu.edu>
2024-07-26 10:55:30 -0400

    results from r5

--- fitting/sims/collect_sims.R
+++ fitting/sims/collect_sims.R
@@ -1,11 +1,16 @@
 
-r <- 5 
+# Compare my timings to Steve's
+annie <- read.csv("results/sims_1_4_5.csv")
+steve <- read.csv("results/STEVE_sims_1_4_5.csv")
+annie[, 6:9]
+steve[, 6:9]
 
+r <- 5 
 par(mfrow = c(2, 4))
 for (func in 1:2) {
   for (setting in 1:4) {
     filename <- paste0("results/sims_", func, "_", setting, "_", r, ".csv")
-    results <- read.csv(filename)[, -1]
+    results <- read.csv(filename)[, 2:5]
     boxplot(results, ylab = "MSE", 
             main = paste0("Function ", func, " Setting ", setting))
   }
--- fitting/sims/run.sh
+++ fitting/sims/run.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-func=1
-setting=1
+func=2
+setting=2
 
-for (( seed=1; seed<=10; seed++ ))
+for (( func=1; func<=2; func++ ))
 do
-  R CMD BATCH "--args func=$func setting=$setting seed=$seed" run_sim.R &
+  for ((setting=1; setting<=4; setting++ ))
+  do
+    for (( seed=1; seed<=20; seed++ ))
+    do
+    R CMD BATCH "--args func=$func setting=$setting seed=$seed" run_sim.R &
+    wait
+    done
+  done
 done
