604938a350405e4f186d655a62779fe3d164be23
Steve Walsh <stevewalsh124@yahoo.com>
2025-06-16 16:34:20 -0400

    rm unused sim settings

--- sims/functions.R
+++ sims/functions.R
@@ -38,16 +38,6 @@ get_Sigma_true <- function(x, n, func = 1, setting = 1) {
                                     theta = 1e-2, g = 1e-8, v = 2.5)
     } else if(setting == 5) {
       D <- deepgp::sq_dist(x)
-      Sigma <- deepgp:::Matern(distmat = D, tau2 = 0.1^2, 
-                               theta = 5e-2, g = 1e-8, v = 2.5)
-      Sigma_true <- diag(sddtrue) %*% Sigma %*% diag(sddtrue)
-    } else if(setting == 6) {
-      D <- deepgp::sq_dist(x)
-      Sigma <- deepgp:::Matern(distmat = D, tau2 = .1, 
-                               theta = 1e-1, g = 1e-8, v = 2.5)
-      Sigma_true <- diag(sddtrue) %*% Sigma %*% diag(sddtrue)
-    } else if(setting == 7) {
-      D <- deepgp::sq_dist(x)
       Sigma <- deepgp:::Matern(distmat = D, tau2 = .1, 
                                theta = 5e-2, g = 1e-8, v = 2.5)
       Sigma_true <- diag(sddtrue) %*% Sigma %*% diag(sddtrue)
@@ -70,16 +60,6 @@ get_Sigma_true <- function(x, n, func = 1, setting = 1) {
       Sigma_true <- deepgp:::Matern(distmat = D, tau2 = 0.15^2, 
                                    theta = 1e-2, g = 1e-8, v = 2.5)
     } else if(setting == 5) {
-      D <- deepgp::sq_dist(x)
-      Sigma <- deepgp:::Matern(distmat = D, tau2 = 0.1^2, 
-                               theta = 5e-2, g = 1e-8, v = 2.5)
-      Sigma_true <- diag(sddtrue) %*% Sigma %*% diag(sddtrue)
-    } else if(setting == 6) {
-      D <- deepgp::sq_dist(x)
-      Sigma <- deepgp:::Matern(distmat = D, tau2 = .1, 
-                               theta = 1e-1, g = 1e-8, v = 2.5)
-      Sigma_true <- diag(sddtrue) %*% Sigma %*% diag(sddtrue)
-    } else if(setting == 7) {
       D <- deepgp::sq_dist(x)
       Sigma <- deepgp:::Matern(distmat = D, tau2 = .1, 
                                theta = 5e-2, g = 1e-8, v = 2.5)
