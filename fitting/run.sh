a379370af5232720d13ac4d619f51bb30531e1ca
annie_booth <annie_booth@ncsu.edu>
2025-06-19 18:30:55 -0400

    all Mira-Titan models rerun

--- fitting/run.sh
+++ fitting/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 model=1
-deep=1
 
-for (( model=12; model<=20; model++ ))
+for (( model=0; model<=116; model++ ))
 do
-  R CMD BATCH "--args model=$model deep=$deep" run_fit.R &
+  R CMD BATCH "--args model=$model" run_fit.R &
+  wait
 done
